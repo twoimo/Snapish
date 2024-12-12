4b84b88ac2d025ec1d6cd75630764831b85a74ff
jh-shi-n <sj124h@naver.com>
2024-12-12 11:37:33 +0900

    temporary disable weather widget - vuex

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,21 +1,25 @@
 import { createStore } from "vuex";
 import { getCurrentLocation } from "../services/locationService";
-import { fetchWeatherByCoordinates } from "../services/weatherService";
+// import { fetchWeatherByCoordinates } from "../services/weatherService";
 
 export default createStore({
   state: {
-    weather: JSON.parse(localStorage.getItem("weather")) || null,
+    // weather: JSON.parse(localStorage.getItem("weather")) || null,
+    currentlocation: null,
     loading: false,
     error: null,
-    lastupdated: (() => {
-      const weatherData = JSON.parse(localStorage.getItem("weather"));
-      return weatherData?.current?.last_updated || null; // 안전하게 last_updated 추출
-    })(),
+    // lastupdated: (() => {
+    //   const weatherData = JSON.parse(localStorage.getItem("weather"));
+    //   return weatherData?.current?.last_updated || null; // 안전하게 last_updated 추출
+    // })(),
   },
   mutations: {
-    setWeather(state, weather) {
-      state.weather = weather;
-      localStorage.setItem("weather", JSON.stringify(weather));
+    // setWeather(state, weather) {
+    //   state.weather = weather;
+    //   localStorage.setItem("weather", JSON.stringify(weather));
+    // },
+    setCurrentLocation(state, currentlocation) {
+      state.currentlocation = currentlocation;
     },
     setLoading(state, isLoading) {
       state.loading = isLoading;
@@ -23,46 +27,60 @@ export default createStore({
     setError(state, error) {
       state.error = error;
     },
-    setLastUpdated(state, lastUpdated) {
-      state.lastupdated = lastUpdated;
-      localStorage.setItem("lastupdated", lastUpdated);
-    },
+    // setLastUpdated(state, lastUpdated) {
+    //   state.lastupdated = lastUpdated;
+    //   localStorage.setItem("lastupdated", lastUpdated);
+    // },
   },
   actions: {
-    async fetchWeather({ commit }) {
-      console.log("vuex : fetchWeather action triggered."); // 액션 호출 확인
+    async fetchLocation({ commit }) {
+      console.log("vuex : fetchLocation action triggered."); // 액션 호출 확인
       commit("setLoading", true);
       commit("setError", null);
 
       try {
         const { latitude, longitude } = await getCurrentLocation();
-        const weatherData = await fetchWeatherByCoordinates(latitude, longitude);
 
-        if (weatherData) {
-          const lastUpdated = weatherData.current.last_updated;
-          console.log(`check: ${ lastUpdated } `)
-          commit("setWeather", weatherData);
-          commit("setLastUpdated", lastUpdated);
-        } else {
-          commit("setError", "No weather data found.");
+        if (latitude && longitude) {
+          const currentloc = [latitude, longitude]
+          commit("setCurrentLocation", currentloc)
+        }else {
+          commit("setError", "No Location data found.");
         }
-      } catch (error) {
-        const defaultLat = 37.5665; // 위치 권한을 받아올 수 없는 경우
-        const defaultLon = 126.9780;
-        try {
-          const weatherData = await fetchWeatherByCoordinates(defaultLat, defaultLon);
-          if (weatherData) {
-            commit("setWeather", weatherData); // 서울 데이터 저장
-            commit("setLastUpdated", weatherData.last_updated); // 현재 시간을 last_updated에 저장
-          } else {
-            commit("setError", "No fallback weather data found.");
-          }
-        } catch (fallbackError) {
-          commit("setError", "Failed to fetch weather data.");
+        } catch (error) {
+          commit("setError", error);
+        } finally {
+          commit("setLoading", false);
         }
-      } finally {
-        commit("setLoading", false);
-      }
-    }
-  },
-});
+      },
+    },
+    //     const weatherData = await fetchWeatherByCoordinates(latitude, longitude);
+
+    //     if (weatherData) {
+    //       const lastUpdated = weatherData.current.last_updated;
+    //       console.log(`check: ${ lastUpdated } `)
+    //       commit("setWeather", weatherData);
+    //       commit("setLastUpdated", lastUpdated);
+    //     } else {
+    //       commit("setError", "No weather data found.");
+    //     }
+    //   } catch (error) {
+    //     const defaultLat = 37.5665; // 위치 권한을 받아올 수 없는 경우
+    //     const defaultLon = 126.9780;
+    //     try {
+    //       const weatherData = await fetchWeatherByCoordinates(defaultLat, defaultLon);
+    //       if (weatherData) {
+    //         commit("setWeather", weatherData); // 서울 데이터 저장
+    //         commit("setLastUpdated", weatherData.last_updated); // 현재 시간을 last_updated에 저장
+    //       } else {
+    //         commit("setError", "No fallback weather data found.");
+    //       }
+    //     } catch (fallbackError) {
+    //       commit("setError", "Failed to fetch weather data.");
+    //     }
+    //   } finally {
+    //     commit("setLoading", false);
+    //   }
+    // }
+  }
+)
