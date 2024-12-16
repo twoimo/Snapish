ee5d776bc166c48b4fa849b6d956225fb219c3c7
twoimo <twoimo@dgu.ac.kr>
2024-12-16 20:35:16 +0900

    Enhance Catches and Home views with improved layout, loading indicators, and reverse order for displayed catches

--- frontend/src/views/Catches.vue
+++ frontend/src/views/Catches.vue
@@ -1,19 +1,18 @@
 <template>
-    <div class="min-h-screen bg-gray-100 flex justify-center p-4">
-        <div class="w-full max-w-4xl bg-white shadow-lg rounded-lg overflow-hidden">
-            <main class="pb-20 px-4">
-                <section class="mb-6 pt-4">
-                    <div v-if="displayedCatches.length > 0" class="grid grid-cols-2 gap-4">
-                        <div v-for="(catchItem, index) in displayedCatches" :key="index"
-                            class="bg-gray-50 p-4 rounded-lg shadow-sm">
-                            <img :src="catchItem.imageUrl" alt="Catch Image"
-                                class="w-full h-32 object-cover rounded-lg mb-2" />
-                            <p class="text-gray-800 text-sm">{{ catchItem.detections }}</p>
-                        </div>
+    <div class="min-h-screen bg-gray-100 flex justify-center">
+        <div class="w-full max-w-4xl bg-white shadow-lg">
+            <main class="p-4">
+                <div v-if="catches.length > 0" class="grid grid-cols-2 gap-4">
+                    <div v-for="(catchItem, index) in displayedCatches.slice().reverse()" :key="index"
+                        class="bg-gray-50 p-4 rounded-lg shadow-sm">
+                        <img :src="catchItem.imageUrl" alt="Catch Image"
+                            class="w-full h-32 object-cover rounded-lg mb-2" />
+                        <p class="text-gray-800 text-sm">{{ catchItem.detections }}</p>
                     </div>
-                    <div v-else class="text-gray-500 flex flex-col items-center p-2">아직 잡은 물고기가 없습니다.</div>
-                    <div ref="loadMoreTrigger" class="h-1"></div>
-                </section>
+                    <div ref="loadMoreTrigger" class="col-span-2 h-1"></div>
+                </div>
+                <div v-else class="text-gray-500">아직 잡은 물고기가 없습니다.</div>
+                <div v-if="loading" class="text-center text-gray-500 mt-4">Loading...</div>
             </main>
         </div>
     </div>
@@ -67,34 +66,5 @@ onMounted(() => {
     display: grid;
     grid-template-columns: repeat(2, 1fr);
     gap: 1rem;
-}
-
-* {
-    -ms-overflow-style: none;
-    /* IE and Edge */
-    scrollbar-width: none;
-    /* Firefox */
-}
-
-*::-webkit-scrollbar {
-    display: none;
-    /* Chrome, Safari, Opera*/
-}
-
-.touch-pan-x {
-    -webkit-overflow-scrolling: touch;
-    overflow-scrolling: touch;
-}
-
-.scroll-smooth {
-    scroll-behavior: smooth;
-}
-
-.hover\:bg-gray-50:hover {
-    background-color: #f9fafb;
-}
-
-.transition {
-    transition: background-color 0.3s ease;
 }
 </style>
