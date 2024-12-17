331e93257c35dfe8ea75b1ae9d491d9387c9007b
jh-shi-n <sj124h@naver.com>
2024-12-17 17:26:28 +0900

    Add button : Check Location in KakaoAPI

--- frontend/src/components/MapLocationDetail.vue
+++ frontend/src/components/MapLocationDetail.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="details-content">
-    <button class="close-btn" @click="$emit('close')">닫기</button>  
+    <div class="button-container">
+      <button class="check-btn" >위치 확인</button> 
+      <button class="close-btn" @click="$emit('close')">닫기</button> 
+    </div>
+
+    <br>
     <section class=" mb-6 pt-4">   
       <div class="place-name"> {{ location.location_id }} </div>
       <p><strong>주소:</strong> {{ location.address_ko }}</p>
@@ -37,6 +42,28 @@ export default {
   max-height: 100%;
 }
 
+/* 버튼을 오른쪽 끝으로 이동 */
+.button-container {
+  display: flex;
+  justify-content: flex-end; /* 오른쪽 정렬 */
+  gap: 10px; /* 버튼 사이 간격 */
+}
+
+/* 위치 확인 버튼 스타일 */
+.check-btn {
+  margin-top: 10px;
+  padding: 5px 10px;
+  background-color: #4caf50; /* 초록색 */
+  color: white;
+  border: none;
+  cursor: pointer;
+  border-radius: 5px;
+}
+
+.check-btn:hover {
+  background-color: #45a045; /* 진한 초록색 */
+}
+
 .close-btn {
   margin-top: 10px;
   padding: 5px 10px;
@@ -45,6 +72,7 @@ export default {
   border: none;
   cursor: pointer;
   border-radius: 5px;
+  float: right;
 }
 
 .close-btn:hover {
