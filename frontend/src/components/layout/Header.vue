4a0a8eef32fcd7f16da8363861047dc9919545e4
jh-shi-n <sj124h@naver.com>
2024-12-30 00:36:23 +0900

    Add Logo image - snapish

--- frontend/src/components/layout/Header.vue
+++ frontend/src/components/layout/Header.vue
@@ -3,7 +3,9 @@
     <!-- 네비게이션 바 -->
     <nav class="container mx-auto px-4 py-3 flex justify-between items-center">
       <!-- 사이트 제목 -->
-      <h1 class="text-xl font-bold cursor-pointer" @click="$router.push('/')">SNAPISH</h1>
+      <h1 class="cursor-pointer" @click="$router.push('/')">
+        <img src="/header_snapish.png" alt="SNAPISH" class="h-10 w-50" />
+      </h1>
       <div class="flex items-center">
         <button class="p-2" @click="handleLogout" title="로그아웃">
           <LogOutIcon class="w-6 h-6" />
