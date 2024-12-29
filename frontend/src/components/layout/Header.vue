d2a469888529c185c15286100ea8974e387e5444
jh-shi-n <sj124h@naver.com>
2024-12-30 00:41:23 +0900

    Add login logout icon by following each situration

--- frontend/src/components/layout/Header.vue
+++ frontend/src/components/layout/Header.vue
@@ -7,8 +7,13 @@
         <img src="/header_snapish.png" alt="SNAPISH" class="h-10 w-50" />
       </h1>
       <div class="flex items-center">
-        <button class="p-2" @click="handleLogout" title="로그아웃">
-          <LogOutIcon class="w-6 h-6" />
+        <button 
+          class="p-2" 
+          @click="isLoggedIn ? handleLogout() : handleLogin()" 
+          :title="isLoggedIn ? '로그아웃' : '로그인'"
+        >
+          <LogInIcon v-if="!isLoggedIn" class="w-6 h-6" />
+          <LogOutIcon v-else class="w-6 h-6" />
         </button>
       </div>
     </nav>
@@ -16,26 +21,36 @@
 </template>
 
 <script>
-import { LogOutIcon } from 'lucide-vue-next'
+import { LogInIcon, LogOutIcon } from 'lucide-vue-next'
 import { useStore } from 'vuex'
 import { useRouter } from 'vue-router'
+import { computed } from 'vue'
 
 export default {
   name: 'AppHeader',
   components: {
+    LogInIcon,
     LogOutIcon
   },
   setup() {
     const store = useStore()
     const router = useRouter()
 
+    const isLoggedIn = computed(() => store.getters.isAuthenticated)
+
     const handleLogout = async () => {
       await store.dispatch('logout')
       router.push('/login')
     }
 
+    const handleLogin = () => {
+      router.push('/login')
+    }
+
     return {
-      handleLogout
+      isLoggedIn,
+      handleLogout,
+      handleLogin
     }
   }
 }
